d8e889753cc4de762927fc44f829fda252413594
Christina Delpone <cdelpone@gmail.com>
2022-04-11 12:10:21 -0600

    Write user/mircopost relationship model tests

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    # it { should validate_presence_of(:content) }
+    # it { should validate_presence_of(:user_id) }
+  end
+
+  describe 'class methods' do
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:microposts) }
+  end
+
+  describe 'validations' do
+    # it { should validate_presence_of(:email) }
+    # it { should validate_presence_of(:name) }
+    # it { should validate_uniqueness_of(:email) }
+  end
+
+  describe 'class methods' do
+  end
 end
